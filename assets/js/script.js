af90de4b8f7346402dfa271f97835545c3013625
John Netzel <johnrnetzel@gmail.com>
2022-04-09 06:04:38 -0800

    Add check for duplicate search names when inserting into city search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+const citySearchHistory = [];
+
 const citySearchFormEl = $("#city-search-form");
 const citySearchInputEl = $("#city-search-input");
 const citySearchHistoryEl = $("#city-search-history");
@@ -5,11 +7,16 @@ const citySearchHistoryEl = $("#city-search-history");
 citySearchFormEl.on("submit", function(event)
 {
     event.preventDefault(); // default behavior is to reload page
-    addCityToSearchHistory(citySearchInputEl.val());
+
+    var cityName = citySearchInputEl.val();
+    if(citySearchHistory.indexOf(cityName) === -1) // If city name is not present in search history
+        addCityToSearchHistory(cityName);
 });
 
 function addCityToSearchHistory(cityName)
 {
+    citySearchHistory.push(cityName);
+
     var cityEl = $("<div>");
     
     var cityNameEl = $("<p>");
