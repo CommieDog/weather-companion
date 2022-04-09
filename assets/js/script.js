ffb9b2d19bdf5491e591b362134023644c80adaa
John Netzel <johnrnetzel@gmail.com>
2022-04-09 05:56:43 -0800

    Add searched cite names to search history below search form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,20 @@
 const citySearchFormEl = $("#city-search-form");
 const citySearchInputEl = $("#city-search-input");
+const citySearchHistoryEl = $("#city-search-history");
 
 citySearchFormEl.on("submit", function(event)
 {
     event.preventDefault(); // default behavior is to reload page
-    alert(citySearchInputEl.val());
-})
+    addCityToSearchHistory(citySearchInputEl.val());
+});
+
+function addCityToSearchHistory(cityName)
+{
+    var cityEl = $("<div>");
+    
+    var cityNameEl = $("<p>");
+    cityNameEl.text(cityName);
+    cityNameEl.appendTo(cityEl);
+
+    cityEl.appendTo(citySearchHistoryEl);
+}
