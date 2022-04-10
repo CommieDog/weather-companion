e064a102561440b0462297a2e4a729074c15a31a
John Netzel <johnrnetzel@gmail.com>
2022-04-10 14:54:20 -0800

    Fix styling of city search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,13 +14,11 @@ const forecastDay4El = $("#forecast-day-4");
 const forecastDay5El = $("#forecast-day-5");
 const forecastEls = [forecastDay1El, forecastDay2El, forecastDay3El, forecastDay4El, forecastDay5El];
 
-var cityName;
-
 citySearchFormEl.on("submit", function(event)
 {
     event.preventDefault(); // default behavior is to reload page
 
-    cityName = citySearchInputEl.val();
+    var cityName = citySearchInputEl.val();
     if(citySearchHistory.indexOf(cityName) === -1) // If city name is not present in search history
     {
         addCityToSearchHistory(cityName);
@@ -44,30 +42,24 @@ citySearchFormEl.on("submit", function(event)
     });
 });
 
-citySearchHistoryEl.on("click", ".delete-icon", function()
+citySearchHistoryEl.on("click", ".btn-close", function()
 {
-    var cityName = $(this).siblings(".city-label").text();
+    var cityName = $(this).parent().text();
     var cityNameIndex = citySearchHistory.indexOf(cityName);
     citySearchHistory.splice(cityNameIndex, 1) // Removes the city name from the search history
 
-    $(this).parents(".card").remove(); // Removes the city card from the HTML document
+    $(this).parent().remove(); // Removes the city name from the HTML document
 });
 
 function addCityToSearchHistory(cityName)
 {
     citySearchHistory.push(cityName);
 
-    var cityEl = $("<div class='card my-2'>");
-    var cityCardBodyEl = $("<div class='card-body row'>");
-    cityCardBodyEl.appendTo(cityEl);
-    
-    var cityNameEl = $("<p class='city-label card-text mb-0 col-11'>");
-    cityNameEl.text(cityName);
-    cityNameEl.appendTo(cityCardBodyEl);
+    var cityEl = $("<li class='list-group-item'>");
+    cityEl.text(cityName);
 
-    var deleteIcon = $("<div class='delete-icon col-1'>");
-    deleteIcon.html("<i class='bi bi-x'></i>");
-    deleteIcon.appendTo(cityCardBodyEl);
+    var deleteIcon = $("<button class='btn-close float-end'>");
+    deleteIcon.appendTo(cityEl);
 
     cityEl.appendTo(citySearchHistoryEl);
 }
